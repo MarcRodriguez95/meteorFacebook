55fe120a891e86d5e74faf06fd466f3ed9a4a93a
Marc <marc.rodriguez451@gmail.com>
2017-04-25 17:40:55 +0200

    first commit

---
+++ client/views/story/story.js
@@ -0,0 +1,132 @@
+Template.story.events({
+    'click .like':function(e) {
+        e.preventDefault();
+        var story = Blaze.getData(e.currentTarget);
+        var liker = Meteor.user();
+        var likeData = {name: liker.profile.name.first + " " + liker.profile.name.last};
+        var alreadyLiked = _.findWhere(story.likes, likeData);
+        if(!alreadyLiked){
+            Stories.update({_id: story._id}, {$push:{likes: likeData}});
+        } else {
+            Stories.update({_id: story._id}, {$pull:{likes:likeData}});
+        }
+    },
+    'click .delete-story':function(e){
+      e.preventDefault();
+      var story = Blaze.getData(e.currentTarget);
+      Stories.remove(story._id);
+    },
+    'submit .comment-story':function(event){
+      event.preventDefault();
+
+      const target = event.target;
+      const commentText = target.text.value;
+
+      var story = Blaze.getData(event.currentTarget);
+
+      //Añade a la colección el atributo "Comment"
+      if (commentText){
+        //Usuario del comentario
+        var userOwner = Meteor.user();
+        Stories.update({_id: story._id}, {$push:{comments: {
+            commentText: commentText,
+            commentDate: new Date(),
+            owner: Meteor.userId(),
+            idStory : story._id,
+            userImageComment : userOwner.profile.picture.thumbnail,
+            creatorNameComment : userOwner.profile.name.first,
+			username : userOwner.profile.login.username
+
+         }}});
+        //ordenar los comentarios de las publicaciones
+        Stories.update({_id: story._id},{$push:{comments:{$each:[],$sort: {"commentDate": -1}} }},{'multi':true});
+
+      }
+      target.text.value = '';
+
+    }
+	
+})
+
+Template.story.helpers({
+    status:function(){
+        return this.createdFor === this.createdBy;
+    },
+	ownerStory:function(){
+        return this.createdBy === Meteor.userId();
+    },
+	
+	styleLike:function(){
+
+      var liker = Meteor.user();
+      var likeData = {name: liker.profile.name.first + " " + liker.profile.name.last};
+      var alreadyLiked = _.findWhere(this.likes, likeData);
+      if (! alreadyLiked) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+	
+	datestory:function(date) {
+      return moment(date).format('MM-DD-YYYY HH:mm');
+    },
+	
+    likeCount:function(storyId){
+        var story = Stories.findOne({_id: storyId});
+        var likes = story.likes;
+        if(!likes.length) {
+            return "Nobody has liked this post yet.";
+        } else if(likes.length <= 3) {
+            var string = "";
+            switch (likes.length) {
+                case 1:
+                    return likes[0].name + " likes this";
+                    break;
+                case 2:
+                    return likes[0].name + " and " + likes[1].name + " like this";
+                    break;
+                case 3:
+                    return likes[0].name + ", " + likes[1].name + " and " + likes[2].name + " like this";
+                break;
+            }
+
+        } else {
+            var correctLength = likes.length - 3;
+            var correctOther;
+            if(correctLength === 1) {
+                correctOther = " other person likes this";
+            } else {
+                correctOther = " other people like this";
+            }
+            return likes[0].name + ", " + likes[1].name + ", " + likes[2].name + " and " + correctLength + correctOther;
+        }
+
+    }
+})
+
+Template.commentUsuario.events({
+  'click .delete-comment':function(e){
+    e.preventDefault();
+    var comment = Blaze.getData(e.currentTarget);
+    Stories.update({_id: comment.idStory}, {$pull: { comments: {
+        commentText: comment.commentText ,
+        commentDate: comment.commentDate,
+        owner: comment.owner,
+        idStory: comment.idStory,
+        userImageComment: comment.userImageComment,
+		CreatorNameComment: comment.creatorNameComment,
+        }}});
+  }
+})
+
+
+Template.commentUsuario.helpers({
+  dateRefactor:function(date){
+
+    return moment(date).format('MM-DD-YYYY HH:mm');
+  },
+  ownerComment:function(){
+      return this.owner === Meteor.userId();
+  }
+})
---
+++ server/lib/startup.js
@@ -0,0 +1,30 @@
+Meteor.startup(function(){
+
+    var users = Meteor.users.find().count();
+    if(!users) {
+        for(var i = 0; i < 100; i++){
+
+          HTTP.call('GET', 'https://randomuser.me/api', {
+            data: { some: 'json', stuff: 1 }
+          }, function(err,res) {
+            var fields = res.data.results[0];
+           delete fields.login.salt;
+            delete fields.login.md5;
+            delete fields.login.sha1;
+            delete fields.login.sha256;
+            delete fields.registered;
+            delete fields.dob;
+            delete fields.login.password;
+            var user = {
+                username: fields.login.username,
+                email: fields.email,
+                password:"password",
+                profile: fields
+            }
+            Accounts.createUser(user);
+          });
+        }
+    }
+
+
+})
